da22b3cc3a68adf894ae912ed302cdae8e052c1c
QuameOphory <quameophory@yahoo.com>
2022-06-08 20:44:37 +0000

    add media configurations

--- hotelsystem/settings.py
+++ hotelsystem/settings.py
@@ -114,6 +114,11 @@ USE_I18N = True
 USE_TZ = True
 
 
+#media configurations
+MEDIA_ROOT = os.path.join(BASE_DIR, 'photos/')
+MEDIA_URL = 'photos/'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
