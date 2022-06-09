4a0fd371d1c4d7a36c66f3fa498a0c7b49ba7c29
QuameOphory <quameophory@yahoo.com>
2022-06-09 00:01:22 +0000

    add a listview to display rooms

--- hotel/views.py
+++ hotel/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Room
 # Create your views here.
+
+
+class RoomListView(generic.ListView):
+    model = Room
