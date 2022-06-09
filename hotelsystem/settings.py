caf4898de61e8b0b7adc9b67ddda76c7073c4604
QuameOphory <quameophory@yahoo.com>
2022-06-09 00:32:19 +0000

    add basic html files, update template structure

--- hotelsystem/settings.py
+++ hotelsystem/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'hotelsystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/'), os.path.join(BASE_DIR, 'templates/extras')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
