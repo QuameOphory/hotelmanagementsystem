04cd9ebb78b2a34098f687995115d18d94a02746
QuameOphory <quameophory@yahoo.com>
2022-06-10 17:48:23 +0000

    update room detail page

--- hotel/urls.py
+++ hotel/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RoomListView, RoomCreateView, RoomDetailView
 
 urlpatterns = [
-    path('', RoomListView.as_view(), name='room_home'),
+    path('', RoomListView.as_view(), name='room_list'),
     path('add/', RoomCreateView.as_view(), name='room_create'),
     path('<slug>/', RoomDetailView.as_view(), name='room_detail'),
 ]
