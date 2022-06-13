1cc05708cee9533897a0f5de52b1230e0d892286
QuameOphory <quameophory@yahoo.com>
2022-06-13 00:36:45 +0000

    add booking model

--- number_generator.py
+++ number_generator.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+
 def generate_number(prefix, qs, postfix='1', length=4):
     if qs.count() == 0:
         return prefix + postfix.zfill(length - 1)
@@ -5,4 +8,21 @@ def generate_number(prefix, qs, postfix='1', length=4):
         firstrecord = qs.first()
         postfix = str(int(firstrecord.roomnumber[1:]) + 1)
         return prefix + postfix.zfill(length - len(postfix))
+
+
+def generate_number_with_date(prefix, qs, postfix='1'):
+    today = date.today()
+    yy, mm, dd = str(today).split('-')
+    if len(mm) == 1:
+        mm = mm.zfill(2)
+    if len(dd) == 1:
+        dd = dd.zfill(2)
+    if qs.count()==0:
+        return prefix + yy + mm + dd + postfix.zfill(4)
+    else:
+        # TODO: work on it well
+        firstrecord = qs.first()
+        postfix = str(int(firstrecord.bookingnumber[7:]) + 1)
+        return prefix + yy[2:] + mm + dd + postfix.zfill(3)
+    
         
--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from hotel.models import Room
+from uuid import uuid4
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Booking(models.Model):
+    """Model definition for Booking."""
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    bookingnumber = models.CharField(_("Booking Number"), max_length=120)
+    # TODO: change user to clients
+    bookingby = models.ForeignKey(User, verbose_name=_("Client"), on_delete=models.CASCADE)
+    bookingroom = models.ForeignKey(Room, verbose_name=_("Room"), on_delete=models.CASCADE)
+    bookingfrom = models.DateTimeField(_("From"), default=timezone.now)
+    bookingto = models.DateTimeField(_("To"), default=timezone.now)
+    bookingconfirm = models.BooleanField(_("Booking Confirmed"), default=False)
+
+
+    class Meta:
+        """Meta definition for Booking."""
+
+        verbose_name = 'Booking'
+        verbose_name_plural = 'Bookings'
+
+    def __str__(self):
+        """Unicode representation of Booking."""
+        pass
