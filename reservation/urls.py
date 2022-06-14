5bcb405103b99d966dcdbc54de9b7ed59fac3394
QuameOphory <quameophory@yahoo.com>
2022-06-14 10:46:49 +0000

    update room detail html

--- reservation/urls.py
+++ reservation/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import BookingCreateView
 
 urlpatterns = [
-    path('', BookingCreateView.as_view(), name='booking_list'),
+    path('', BookingCreateView.as_view(), name='booking_create'),
 ]
